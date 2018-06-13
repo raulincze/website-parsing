bc307b98e77c39aa533a76c05d454830f466e05c
Raul <raulincze@gmail.com>
2018-06-13 13:07:40 +0300

    Initial commit

---
+++ constants.py
@@ -0,0 +1,16 @@
+LABEL_MAP = {
+    'NON_SECTION': 0,
+    'HEADER': 1,
+    'NAV': 2,
+    'NAVIGATION': 2,
+    'FOOTER': 3,
+    'CONTENT': 4,
+    'CONTROL': 5,
+    'FORM': 6
+}
+
+CLASSES_NAMES = ['N/A', 'HEADER', 'NAV', 'FOOTER', 'CONTENT', 'CONTROL', 'FORM']
+
+TAG_KEYWORDS = ['content', 'main', 'container', 'control', 'sort', 'order', 'filter', 'dropdown', 'checkbox', 'button', 'footer', 'foot', 'form', 'header', 'nav', 'menu', 'navigation', 'navbar']
+
+TAG_ATTRIBUTES = ['class', 'id', 'role', 'type']
